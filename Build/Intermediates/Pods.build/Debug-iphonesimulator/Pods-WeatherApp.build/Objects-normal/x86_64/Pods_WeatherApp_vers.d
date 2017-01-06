5ce797cfa5c444a06729ae24766e68e1395a5046
supensa <stitch@Mobap.local>
2017-01-07 02:25:20 +1100

    Created .gitignore

--- Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-WeatherApp.build/Objects-normal/x86_64/Pods_WeatherApp_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/stitch/Documents/Informatique/iOS/iOS10/6-REST\ Apps/WeatherApp/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-WeatherApp.build/DerivedSources/Pods_WeatherApp_vers.c
